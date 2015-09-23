f17808bb86102882eccb23d00b7ccb49322ce12e
Victor Ortiz <kespers@gmail.com>
2015-09-23 15:13:57 +0200

    Add js generator

---
+++ lib/generators/gintonic/js/js_generator.rb
@@ -0,0 +1,26 @@
+class Gintonic::JsGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+
+  def create_structure
+    create_file "app/assets/javascripts/libs/.keep", ""
+    create_file "app/assets/javascripts/modules/.keep", ""
+  end
+
+  def copy_ready_js
+    template 'ready.js',
+      File.join(destination_root, 'app/assets/javascripts/ready.js')
+  end
+
+  def copy_application_js
+    template 'application.js',
+      File.join(destination_root, 'app/assets/javascripts/application.js')
+  end
+
+  def install_jquery_turbolinks
+    begin
+      gem 'jquery-turbolinks'
+    rescue Errno::ENOENT
+      say_status("warning", "It seems you don't have any Gemfile. I hope you are in the dummy app.", :yellow)
+    end
+  end
+end
--- lib/gintonic-rails/engine.rb
+++ lib/gintonic-rails/engine.rb
@@ -1,9 +1,13 @@
+require 'bourbon'
+
 module Gintonic
   module Rails
     class Engine < ::Rails::Engine
+      isolate_namespace Gintonic
+
       initializer 'gintonic.assets.precompile' do |app|
-        %w(stylesheets javascripts fonts images).each do |sub|
-          app.config.assets.paths << root.join('assets', sub).to_s
+        %w(stylesheets javascripts fonts images).each do |dir|
+          app.config.assets.paths << root.join('assets', dir).to_s
         end
       end
     end
